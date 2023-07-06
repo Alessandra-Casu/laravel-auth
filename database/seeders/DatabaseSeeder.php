c705fe2c73ca23f4f3d9050e2d9279a88338463f
Alessandra-Casu <alessandracasu919@gmail.com>
2023-07-06 15:20:38 +0200

    add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         
-        // $this->call([
-        //     ProjectsTableSeeder::class,
-        // ]);
+        $this->call([
+            ProjectsTableSeeder::class,
+        ]);
     }
 }
